7bdb7a6eaf7a0c9dc0fc19d146dfbb76e2d72cd2
suveshmoza <anmolmoza2@gmail.com>
2023-09-19 20:46:41 +0530

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-// You can split your components
-import Editor from "./Editor";
+import Editor from './Editor';
+import Header from './components/Header/Header';
 
 const App = () => {
-  return (
-    <div className="App">
-      {/* Feel free to delete the header */}
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. (Delete me if you
-          like!)
-        </p>
-      </header>
-      <Editor />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Editor />
+		</div>
+	);
 };
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { HiMenu } from 'react-icons/hi';
+
+const Header = () => {
+	return (
+		<header className="h-[64px] flex justify-between">
+			<div className="flex justify-center items-center gap-1 p-8">
+				<HiMenu className="text-2xl" />
+				<h4 className="font-bold text-sm">
+					Design Board
+					<span className="font-light text-[#9196A6] ml-1">(Draft)</span>
+				</h4>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
